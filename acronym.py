3977fab4177da1a6f5e94bd5b7d6d07055412d00
Himanshu Yadav <97745121+Himanshuhi@users.noreply.github.com>
2023-03-06 19:02:21 +0530

    Update and rename acronym-to-json file.py to acronym.py

--- acronym.py
+++ acronym.py
@@ -1,36 +1,30 @@
 import json
 
-# Dictionary to store phrases and their acronyms
+# himanshu
 acronyms = {}
 
-# Load existing acronyms from file
 try:
     with open("acronyms.json", "r") as f:
         acronyms = json.load(f)
 except FileNotFoundError:
     pass
 
-# Function to generate acronym from a phrase
 def generate_acronym(phrase):
     acronym = ""
     for word in phrase.split():
         acronym += word[0].upper()
     return acronym
 
-# Function to print phrase given an acronym
 def print_phrase(acronym):
     if acronym in acronyms:
         print(acronyms[acronym])
     else:
         print("No phrase found for acronym", acronym)
 
-# Main program loop
 while True:
-    # Prompt user for action
     action = input("Enter 'a' to add a phrase, 'g' to generate an acronym, or 'p' to print a phrase: ")
     
     if action == "a":
-        # Prompt user for phrase and store in dictionary
         phrase = input("Enter a phrase: ")
         acronym = generate_acronym(phrase)
         acronyms[acronym] = phrase
@@ -39,16 +33,13 @@ while True:
         print("Acronym added:", acronym)
         
     elif action == "g":
-        # Prompt user for phrase and generate acronym
         phrase = input("Enter a phrase: ")
         acronym = generate_acronym(phrase)
         print("Generated acronym:", acronym)
         
     elif action == "p":
-        # Prompt user for acronym and print phrase if found
         acronym = input("Enter an acronym: ")
         print_phrase(acronym)
         
     else:
-        # Invalid input
         print("Invalid input. Please enter 'a', 'g', or 'p'.")
